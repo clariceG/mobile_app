5934889ee29857a5aa81978a519d5a1516b0f419
clarice <claricegg02@gmail.com>
2023-09-25 14:02:49 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,57 @@
+//Task 1-3
+//fun main(args: Array<String>){
+//    println("What is your name?")
+//    val name = readln()
+//
+//    println("Hello, $name")
+//}
+
+//Task 4-
+//fun main(args: Array<String>){
+//    print("Enter Number: ")
+//    val stringInput = readLine()!!
+//    var integer:Int = stringInput.toInt()
+//
+//    println("You have entered: $integer")
+//}
+
+//Task 5
+//fun main(args: Array<String>){
+//    val first:Int = 10
+//    val second:Int = 20
+//    val sum = first +second
+//    println("the sum is: $sum")
+//}
+
+//Task 6
+//fun main(args: Array<String>){
+//    val ch = "j"
+//    val vowelConsonant = if (ch =="a"||ch=="e"||ch=="i"||ch=="0"||ch=="u") "vowel" else "consonant"
+//    println("$ch is $vowelConsonant")
+//}
+
+//Task 7
+//fun main(args: Array<String>){
+//    val ch = 'z'
+//    when (ch) {'a','e','i','o','u' -> println("$ch is vowel")
+//        else -> println("$ch is consonant")
+//    }
+//}
+
+//Task 8
+//fun main(args: Array<String>){
+//    val year = 1890
+//    var leap = false
+//    if(year % 4 == 0){
+//        if (year % 100 == 0){
+//            leap= year % 400 == 0
+//        } else
+//            leap = true
+//    }else
+//        leap = false
+//    println(if (leap) "$year is a leap year." else "$year" +
+//            "is not a leap year.")
+//}
+
+
+
